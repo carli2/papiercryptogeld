dd080021b7a8aa82d4df42fb29759faf533c3eef
Carl-Philip Hänsch <carli@launix.de>
2017-08-26 21:45:48 +0200

    Geldscheine anzeigen

--- js/main.js
+++ js/main.js
@@ -13,4 +13,24 @@ app.controller('Main', function ($scope) {
 		$scope.$apply();
 	}
 	$scope.qrtext = 'Hello World';
+
+	$scope.pocket = [
+		{pub: 'pub1', amount: 1.3, priv: 'priv1'},
+		{pub: 'pub2', amount: 0.7, priv: 'priv2'},
+		{pub: 'pub3', amount: 6, priv: 'priv3'}
+	];
+});
+
+app.directive('bill', function () {
+	return {
+		restrict: 'E',
+		scope: { ngModel: '=' },
+		controller: function ($scope) {
+			$scope.bank = 'Werkraum Zittau e.V.';
+			$scope.bankurl = 'https://launix.de/';
+		},
+		template: '<div class="bill"><qrcode size="100" data="{{ngModel.pub}}"></qrcode><div class="main">'
+		+'Seriennummer: {{ngModel.pub}}<br><span class="wert">Wert: {{ngModel.amount|number:2}}</span><br>{{bank}}<br><a ng-href="bankurl" target="_blank">{{bankurl}}</a>'
+		+'</div><div class="scramble" ng-if="ngModel.priv"></div><div class="trenner" ng-if="ngModel.priv"><span>hier knicken</span></div><qrcode size="100" data="{{ngModel.pub}}" ng-if="ngModel.priv"></qrcode></div>'
+	};
 });
