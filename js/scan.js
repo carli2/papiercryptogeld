af32aaf0de0c6a357dc1cbd4bdb74b21b39309c6
Carl-Philip Hänsch <carli@launix.de>
2017-08-27 20:41:58 +0200

    Back-Kamera bevorzugen

--- js/scan.js
+++ js/scan.js
@@ -3,7 +3,11 @@ $(function () {
 	scanner.addListener('scan', onScan);
 	Instascan.Camera.getCameras().then(function (cameras) {
 		if (cameras.length > 0) {
-			scanner.start(cameras.pop());
+			var favourite = 0;
+			for (var i = 0; i < cameras.length; i++) {
+				if (cameras[i].name.indexOf('back') != -1) favourite = i;
+			}
+			scanner.start(cameras[favourite]);
 		} else {
 			console.error('No Cameras found');
 		}
