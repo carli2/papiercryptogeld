262877d5ea3a661ea21b3c2bcc3d5d8bf234692f
Carl-Philip Hänsch <carli@launix.de>
2017-08-27 14:22:57 +0200

    optimiere Druck-Ansicht

--- js/tokens.js
+++ js/tokens.js
@@ -82,7 +82,7 @@ if (typeof app !== 'undefined') {
 			},
 			template: '<div class="bill" ng-class="{selected: ngModel.selected}" ng-click="ngModel.selected = !ngModel.selected"><qrcode version="4" size="400" data="pub:{{ngModel.pub}}"></qrcode><div class="main">'
 			+'Seriennummer: {{ngModel.pub}}<br><span class="wert"><span ng-if="ngModel.amount > 0">Wert: {{ngModel.amount|money}}</span><span ng-if="!(ngModel.amount > 0)" class="invalid">unbekannt / ungültig</span></span><br>{{bank}}<br><a ng-href="bankurl" target="_blank">{{bankurl}}</a>'
-			+'</div><img src="scramble.png" class="scramble" ng-if="ngModel.priv && !hidePriv"><div class="trenner" ng-if="ngModel.priv && !hidePriv"><span>hier knicken</span></div><qrcode version="4" size="400" data="priv:{{ngModel.priv}}" ng-if="ngModel.priv && !hidePriv"></qrcode></div>'
+			+'</div><div class="trenner" ng-if="ngModel.priv && !hidePriv"><span>hier knicken</span></div><img src="scramble.png" class="scramble" ng-if="ngModel.priv && !hidePriv"><div class="trenner" ng-if="ngModel.priv && !hidePriv"><span>hier knicken</span></div><qrcode version="4" size="400" data="priv:{{ngModel.priv}}" ng-if="ngModel.priv && !hidePriv"></qrcode></div>'
 		};
 	});
 
